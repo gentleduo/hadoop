400e86f1c6509e441240870d27c719f3b0fb6a01
gentleduo <gentleduo@163.com>
2022-08-30 17:18:32 +0800

    custom grouping commit

--- src/main/java/org/duo/grouping/OrderPartition.java
+++ src/main/java/org/duo/grouping/OrderPartition.java
@@ -5,9 +5,10 @@ import org.apache.hadoop.mapreduce.Partitioner;
 
 public class OrderPartition extends Partitioner<OrderBean, Text> {
 
-    //分区规则: 根据订单的ID实现分区
-
     /**
+     * 分区规则: 根据订单的ID实现分区
+     * 如果不自定义OrderPartition的话，默认会按照orderBean的hashCode来分区，那么相同的订单id就会被分在不同的分区
+     *
      * @param orderBean K2
      * @param text      V2
      * @param i         ReduceTask个数
